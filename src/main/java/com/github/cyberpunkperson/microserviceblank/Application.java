f471672a7a75e54eca12aae450617fd433c1dd0b
Andrew Belunov <cyberpunk.person@gmail.com>
2022-03-19 01:21:00 +0300

    Init

--- src/main/java/com/github/cyberpunkperson/microserviceblank/Application.java
+++ src/main/java/com/github/cyberpunkperson/microserviceblank/Application.java
@@ -2,8 +2,15 @@ package com.github.cyberpunkperson.microserviceblank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.info.ProjectInfoAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		WebSocketServletAutoConfiguration.class,
+		ProjectInfoAutoConfiguration.class
+})
+@ConfigurationPropertiesScan
 public class Application {
 
 	public static void main(String[] args) {
